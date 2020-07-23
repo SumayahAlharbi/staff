0e05dff6a009650cd4f96e55e196ab093689499c
SumayahAlharbi <ftooo-ff@hotmail.com>
2020-07-23 20:07:25 +0300

    Added date range to the export of the attendance sheet

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -18,7 +18,33 @@
   @endif
 
   @can('export')
-  <div class="float-left"> <a class="btn btn-primary" href="{{ route('attendancesheet.export') }}" role="button">Export</a> </div>
+  <!--<div class="float-left"> <a class="btn btn-primary" href="{{ route('attendancesheet.export') }}" role="button">Export</a> </div>-->
+  <div class="card">
+    <div class="card-body">
+  <form method="post" action="{{ route('attendancesheet.export') }}">
+    <div class="row">
+      <div class="form-group col-md-4">
+          @csrf
+          <label for="from_date">from</label>
+          <input type="date" class="form-control" name="from_date" />
+      </div>
+      <div class="form-group col-md-4">
+        <label for="to_date">to</label>
+        <input type="date" class="form-control" name="to_date" />
+      </div>
+      <div class="col-md-4">
+        <div class="form-group">
+          <label for="sort_by">Sort by</label>
+          <select class="form-control" name="sort_by">
+              <option value="id">id</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Generate & Download</button>
+  </form>
+</div>
+</div>
   @endcan
 
   <table class="table table-striped">

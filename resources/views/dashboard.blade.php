903a303194efe08b6d3bdff6dbdfa8967919c08f
ManalNawar <manal.s.nawar@gmail.com>
2020-08-10 14:41:16 +0300

    Update: dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,23 +6,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Teams & Members</div>
                 <div class="card-body"></div>
               @foreach($groups as $group)
               <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">{{ $group->group_name }}</h4>
+                    <h4 class="card-title ">{{ $group->group_name }}</h4>
+                    <hr class="mt-2 mb-3"/>
                   <p class="card-text">
                     @if($userGroup =$group)
                     @foreach($userGroup->user as $users)
 
-                    <p>{{$users->name}}</p>
+                    <p><a class="badge badge-light" href={{url('/profile/' . $users->id)}}> {{$users->name}}</a></p>
 
                     @endforeach
                     @endif
                   </p>
                 </div>
               </div>
+              <br />
               @endforeach
                 </div>
             </div>

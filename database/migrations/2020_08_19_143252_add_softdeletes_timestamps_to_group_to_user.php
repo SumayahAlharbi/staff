932dce7bfd4a2e88538f4083c672cf92a4068a13
ManalNawar <manal.s.nawar@gmail.com>
2020-08-19 14:35:39 +0300

    Added:soft delete and Timestamps to group_to_user table

---
+++ database/migrations/2020_08_19_143252_add_softdeletes_timestamps_to_group_to_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSoftdeletesTimestampsToGroupToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('group_to_user', function (Blueprint $table) {
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('group_to_user', function (Blueprint $table) {
+            //
+        });
+    }
+}

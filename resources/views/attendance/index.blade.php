18e2e9b7d3269578caab3c1e028bb443e26ec9aa
Ssy3 <ssy.vip@gmail.com>
2020-11-05 12:17:28 +0300

    Fixed: remove livewire from attendancesheet

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -46,8 +46,42 @@
 </div>
 </div>
   @endcan
-  <livewire:update-attendance>
-  
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Group</td>
+          <td>Type</td>
+          <td>Date/Time</td>
+          <td>GPS</td>
+        </tr>
+   
+    </thead>
+    <tbody>
+   
+    @foreach($attendancesheets as $attendancesheet)
+    <tr>
+      <td>{{$attendancesheet->user->name}}</td>
+      <td>{{$attendancesheet->user->email}}</td>
+      <td>{{$attendancesheet->group['group_name']}}</td>
+      <td>{{$attendancesheet->action}}</td>
+      <td>{{$attendancesheet['created_at']}} <b>({{$attendancesheet['created_at']->diffForHumans()}})</b></td>
+      <td><a target="_blank" href="https://www.google.com/search?q={{$attendancesheet->coords}}">Map</a></td>
+  </tr>
+    @endforeach
+
+  </tbody>
+  <tfoot>
+      <tr>
+          <td colspan="6">
+              <div class="text-right">
+                  <ul>   {{ $attendancesheets->links() }} </ul>
+              </div>
+          </td>
+      </tr>
+  </tfoot>
+  </table>
 
 </div>
 {{-- {!! $users->render() !!} --}}

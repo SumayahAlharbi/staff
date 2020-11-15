56d8575f68c186106cbb4dc05e82e90be091f159
Ssy3 <ssy.vip@gmail.com>
2020-11-15 08:13:21 +0300

    Fixed: Typo

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -9,7 +9,7 @@
 
 <p><a href="{{ route('accept', $invite->token) }}">Click here</a> to join!</p>
 <p> Not working? </p>
- <p> 1- Vist: staff.ksauhs.com </p>
+ <p> 1- Visit: staff.ksauhs.com </p>
  <p> 2- Log in </p>
  <p> 3- Go to the Invitations Inbox </p>
  <p> 4- Accept the invitation </p>
